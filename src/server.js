94abd30195f6021ced51ca711ba7e07b58821c41
Your Name <ilham55143@gmail.com>
2024-11-06 16:08:08 +0700

    Menimpa konten notes-app-back-end dengan update lokal

--- src/server.js
+++ src/server.js
@@ -1,31 +1,43 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-unused-vars */
-/* eslint-disable indent */
-/* eslint-disable semi */
-/* eslint-disable no-undef */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable no-empty */
-
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
- 
- 
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
+const ClientError = require('./exceptions/ClientError');
+
 const init = async () => {
-  const server = Hapi.server({
-    port: 5000,
-    host: 'localhost',
-    routes: {
-        cors: {
-            origin: ['*'],
+    const notesService = new NotesService();
+    const server = Hapi.server({
+        port: 5000,
+        host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+        routes: {
+            cors: {
+            origin: ["*"],
+            },
         },
-    },
-  });
-  
-  server.route(routes);
- 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
-};
- 
- 
+    });
+    await server.register({
+        plugin: notes,
+        options: {
+            service: notesService,
+            validator: NotesValidator,
+        },
+    });
+    server.ext("onPreResponse", (request, h) => {
+        // mendapatkan konteks response dari request
+        const { response } = request;
+        // penanganan client error secara internal.
+        if (response instanceof ClientError) {
+            const newResponse = h.response({
+            status: "fail",
+            message: response.message,
+            });
+            newResponse.code(response.statusCode);
+            return newResponse;
+        }
+        return h.continue;
+    });
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+  };
+
 init();
